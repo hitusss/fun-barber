9c43c25e7b2b4ccc51137c3092ed0dc643ca909c
Hitusss <kuba.niespielak@gmail.com>
2022-11-01 14:32:04 +0100

    fix cypress tests

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -2,7 +2,7 @@ describe("smoke tests", () => {
   it("should allow a typical user flow", () => {
     cy.visitAndCheck("/");
 
-    cy.findByRole("link", { name: /blog/i }).click();
+    cy.findAllByRole("link", { name: /blog/i }).first().click();
     cy.location("pathname").should("contain", "/blog").wait(1000);
     cy.findByRole("link", { name: /booking/i }).click();
     cy.location("pathname").should("contain", "booking").wait(1000);

9a272c41e07ddf875b0048b6199a33218e2dcd60
Hitusss <kuba.niespielak@gmail.com>
2023-01-04 16:07:41 +0100

    update Privacy

--- app/routes/privacy.tsx
+++ app/routes/privacy.tsx
@@ -1,18 +1,18 @@
+import { Link } from "@remix-run/react";
 import { Heading } from "~/components/Heading";
 
 export default function Privacy() {
   return (
-    <div className="mx-auto flex max-w-screen-xl flex-col gap-5">
+    <div className="mx-auto my-5 flex max-w-screen-xl flex-col gap-5">
       <Heading>Privacy</Heading>
-      <p>
-        This is only preview site for
-        <br />
-        <a
-          href="https://github.com/Hitusss/fun-barber"
-          className="text-blue-600 underline"
-        >
-          github.com/Hitusss/fun-barber
-        </a>
+      <p className="max-w-prose text-base">
+        The site collect and save only data you provide to booking form, this
+        data is stored in private database and only used for properly site work
+        (to show booked dates on{" "}
+        <Link to="/booking" className="underling text-blue-600">
+          /booking
+        </Link>
+        ).
       </p>
     </div>
   );

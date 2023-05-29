17e530240e8c96afd22d22580f484639590a2be2
Hitusss <kuba.niespielak@gmail.com>
2023-05-29 17:13:51 +0200

    add tos and update privacy

--- app/routes/tos.tsx
+++ app/routes/tos.tsx
@@ -1,9 +1,9 @@
 import { Heading } from "~/components/Heading";
 
-export default function Privacy() {
+export default function TermsOfService() {
   return (
     <div className="mx-auto my-5 flex max-w-screen-xl flex-col gap-5">
-      <Heading>Privacy</Heading>
+      <Heading>Terms Of Service</Heading>
     </div>
   );
 }
